3b9a8a353575ba2b88e5ca02659e318da73cc067
MapManagement <fortnitetest@mail.de>
2020-08-13 20:51:38 +0200

    added new activity for stop watch mode

--- app/src/main/java/com/example/countdown/StopWatchActivity.kt
+++ app/src/main/java/com/example/countdown/StopWatchActivity.kt
@@ -1,2 +1,18 @@
 package com.example.countdown
 
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+
+class StopWatchActivity : AppCompatActivity() {
+
+    var currentColor: String? = "#e01c18"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
