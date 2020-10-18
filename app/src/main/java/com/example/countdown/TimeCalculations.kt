9b378bea467514396130fbf051f8ac4472ed27f1
MapManagement <fortnitetest@mail.de>
2020-10-18 22:53:04 +0200

    added new class for cleaner code (work in progress)

---
+++ app/src/main/java/com/example/countdown/TimeCalculations.kt
@@ -0,0 +1,38 @@
+package com.example.countdown
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.*
+
+class TimeCalculations {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getTimePeriod(chosenDateTime: String?): Long {
+        val format = "yyyy-MM-dd'T'HH:mm:ss"
+        val date = SimpleDateFormat(format, Locale.GERMANY)
+        val currentDateTime = Calendar.getInstance().time
+
+        val formattedCurrentDateTime = date.format(currentDateTime)
+
+        fun LocalDateTime.toMillis(zone: ZoneId = ZoneId.systemDefault()) = atZone(zone).toInstant().toEpochMilli()
+
+        val chosenSeconds: Long = LocalDateTime.parse(chosenDateTime).toMillis() / 1000
+        val currentSeconds: Long = LocalDateTime.parse(formattedCurrentDateTime).toMillis() / 1000
+
+        return chosenSeconds - currentSeconds
+    }
+
+    // converts seconds into different time periods
+    private fun convertSeconds(totalSeconds: Long): ArrayList<Int> {
+        val years = (totalSeconds / 31536000).toInt()
+        val days = ((totalSeconds % 31536000) / 86400).toInt()
+        val hours = (((totalSeconds % 31536000) % 86400) / 3600).toInt()
+        val minutes = ((((totalSeconds % 31536000) % 86400) % 3600) / 60).toInt()
+        val seconds = ((((totalSeconds % 31536000) % 86400) % 3600) % 60).toInt()
+
+        return arrayListOf(years, days, hours, minutes, seconds, totalSeconds.toInt())
+    }
+}
